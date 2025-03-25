df03120791e225ab882ff3f5d4f2071816eae644
Erika-Medeiros <eriikamedeiiros@gmail.com>
2025-03-25 19:34:16 -0300

    faker, variáveis e teste negativo

---
+++ cypress/e2e/3-loja-ebac/cadastro.cy.js
@@ -0,0 +1,48 @@
+/// <reference types="cypress"/>
+import { faker } from '@faker-js/faker';
+
+describe('Funcionalidade: Cadastro', () => {
+
+    beforeEach(() => {
+        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+        
+    });
+
+    it('Deve completar o cadastro com sucesso', () => {
+        cy.get('#reg_email').type(faker.internet.email())
+        cy.get('#reg_password').type('Teste123!')
+        cy.get(':nth-child(4) > .button').click()
+        cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('exist')
+
+        cy.get('.woocommerce-MyAccount-navigation-link--edit-account > a').click()
+        cy.get('#account_first_name').type(faker.person.firstName())
+        cy.get('#account_last_name').type(faker.person.lastName())
+        cy.get('.woocommerce-Button').click()
+        cy.get('.woocommerce-message').should('exist')
+    });
+
+    it('Deve completar o cadastro com sucesso', () => {
+        var firstName = faker.person.firstName()
+        var email = faker.internet.email(firstName)
+        var lastName = faker.person.lastName()
+
+        cy.get('#reg_email').type(email)
+        cy.get('#reg_password').type('Teste123!')
+        cy.get(':nth-child(4) > .button').click()
+        cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('exist')
+
+        cy.get('.woocommerce-MyAccount-navigation-link--edit-account > a').click()
+        cy.get('#account_first_name').type(firstName)
+        cy.get('#account_last_name').type(lastName)
+        cy.get('.woocommerce-Button').click()
+        cy.get('.woocommerce-message').should('exist')
+    });
+
+    it('Deve mostrar mensagem de erro ao inserir email já cadastrado', () => {
+        cy.get('#reg_email').type('erika.teste@gmail.com')
+        cy.get('#reg_password').type('Teste123!')
+        cy.get(':nth-child(4) > .button').click()
+        cy.get('.woocommerce-error').should('contain', 'Erro: Uma conta já está registrada')
+    });
+    
+});
