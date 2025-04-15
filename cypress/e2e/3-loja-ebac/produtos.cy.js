daf7c3b566ccecf8d19049443b07c01215242909
Erika-Medeiros <eriikamedeiiros@gmail.com>
2025-04-15 02:40:15 -0300

    adição de page objects

--- cypress/e2e/3-loja-ebac/produtos.cy.js
+++ cypress/e2e/3-loja-ebac/produtos.cy.js
@@ -22,12 +22,28 @@ describe('Funcionalidade: Produtos', () => {
     });
 
     it('Deve visitar a página do produto', () => {
-        
+        produtosPage.visitarProduto('apollo running short')
+        cy.get('#tab-title-description > a').should('contain', 'Descrição')
     });
 
     it('Deve adicionar produto ao carrinho', () => {
-        
+        let qtd = 8
+        produtosPage.visitarProduto('Frankie Sweatshirt')
+        produtosPage.addProdutoCarrinho('M', 'White', qtd)
+        cy.get('.woocommerce-message').should('contain', 'no seu carrinho.')
     });
 
+    it('Deve adicionar produto ao carrinho buscando da massa de dados', () => {
+        cy.fixture('produtos').then(dados => {
+            produtosPage.visitarProduto(dados[0].nomeProduto)
+            produtosPage.addProdutoCarrinho(
+                dados[0].tamanho, 
+                dados[0].cor, 
+                dados[0].quantidade)
+            cy.get('.woocommerce-message').should('contain', dados[0].nomeProduto)
+        })
+
+        
+    });
 
 });
